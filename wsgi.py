a33abcf1afa2edf7bed37d330358cf146bd11b81
liyifeng <liyifeng@niwodai.net>
2020-01-08 16:20:52 +0800

    适配python3.6

--- wsgi.py
+++ wsgi.py
@@ -14,7 +14,7 @@ __Version__ = "1.2.4"
 __Author__ = "cdhigh <https://github.com/cdhigh>"
 
 from wsgiref.util import is_hop_by_hop
-import os, urllib, urllib2, socket, bottle
+import os, urllib, socket, bottle
 
 ALLOW_KEYS = 'xzSlE'
 
@@ -29,12 +29,12 @@ def Home():
         return 'Auth Key is invalid!'
     
     if url and k:
-        url = urllib.unquote(url.encode('utf-8')).replace(' ', r'%20')
+        url = urllib.parse.unquote(url.encode('utf-8')).replace(' ', r'%20')
         try:
-            req = urllib2.Request(url)
+            req = urllib.request.Request(url)
             req.add_header('User-Agent', "Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3396.99 Safari/537.36")
             req.add_header('Accept', "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8")
-            ret = urllib2.urlopen(req, timeout=timeout)
+            ret = urllib.request.urlopen(req, timeout=timeout)
             content = ret.read()
             headers = [(n,v) for n,v in ret.info().items() if not is_hop_by_hop(n)]
             cookieAdded = False
